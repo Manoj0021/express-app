f94b636b15b4ee9f4453990d43db3bd99165049a
Manoj <manojhn036@gmail.com>
2024-09-26 17:41:54 +0530

    added basic template of express app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app listening at port ${PORT}`);
+});
