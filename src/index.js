018fe9a70a9621349542ee1073c827d370510b33
Manoj <manojhn036@gmail.com>
2024-10-08 14:53:25 +0530

    added config folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const { PORT } = require("./config/server.config");
 
 const app = express();
-
-const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
